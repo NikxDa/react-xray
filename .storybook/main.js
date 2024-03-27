f47c0afb70399bd55003f71e4ac086964ebaddc5
NikxDa <NikxDa@users.noreply.github.com>
2024-03-28 00:08:09 +0100

    Update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,14 @@
 module.exports = {
-    stories: ['../src/**/*.stories.tsx']
+    stories: ['../src/**/*.stories.tsx'],
+
+    docs: {
+        autodocs: true
+    },
+
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {}
+    },
+
+    addons: ['@storybook/addon-webpack5-compiler-swc']
 }
